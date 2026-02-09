097b162d0e73c01ece0f38260e2b4050e90cb082
Ajitama28 <anjitama38@gmail.com>
2026-02-09 13:00:06 +0700

    refactor vite

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
         laravel({
             input: [
                 'resources/css/app.css',
-                'resources/js/public.js',
+                'resources/js/app.js',
             ],
             refresh: true,
         }),
